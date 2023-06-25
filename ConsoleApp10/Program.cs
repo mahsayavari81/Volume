eda008b37467646b910c799300a899c0818a447a
Padidar <Padidar@DESKTOP-NA8IQP4>
2023-06-25 09:46:59 +0330

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter l:");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter h:");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter w:");
+            int w = Convert.ToInt32(Console.ReadLine());
+
+            double show = Calc(l, h, w);
+            Console.WriteLine("\nresualt is: " + show);
+        }
+
+        private static double Calc(int num1, int num2, int num3)
+        {
+            double Result = num1 * num2 * num3;
+            return Result;
+        }
+    }
+}
